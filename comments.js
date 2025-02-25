9467058a375789f4278e77e1b2881288fb432fff
Shaleen-Wonder <shaleenthapa77@gmail.com>
2025-02-25 07:43:47 +0000

    code change

--- comments.js
+++ comments.js
@@ -1,10 +1,10 @@
-const http = require('http');
+import { createServer } from 'http';
 
 //create web server
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!\n');
