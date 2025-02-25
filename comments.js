c3861ff073fa1002d04cf0a6468a0ab681f4a6eb
Shaleen-Wonder <shaleenthapa77@gmail.com>
2025-02-25 07:45:42 +0000

    code change

--- comments.js
+++ comments.js
@@ -1,15 +1,11 @@
-import { createServer } from 'http';
+const http = require("http");
 
-//create web server
-const hostname = '127.0.0.1';
-const port = 3000;
+const host = 'localhost';
+const port = 8000;
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+const requestListener = function (req, res) {};
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
 });
