f10bc841160e5979bbcdbf6f61dd523108f5b227
Shaleen-Wonder <shaleenthapa77@gmail.com>
2025-02-25 08:28:00 +0000

    Create web server

--- comments.js
+++ comments.js
@@ -1,42 +1,43 @@
-// create web server
-var http = require('http');
-// parse url
-var url = require('url');
-// parse querystring
-var querystring = require('querystring');
-// file system module
-var fs = require('fs');
-var comments = [];
+// Create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const qs = require('querystring');
+const comments = require('./comments.json');
 
-function send(res, data) {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    });
-    res.end(JSON.stringify(data));
-}
+//Create web server
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url);
+    const parsedQuery = qs.parse(parsedUrl.query);
+    const resourseName = path.basename(parsedUrl.pathname);
 
-var server = http.createServer(function(req, res) {
-    var parsedUrl = url.parse(req.url);
-    var parsedQuery = querystring.parse(parsedUrl.query);
-    var pathname = parsedUrl.pathname;
-    if (pathname == '/addComment') {
-        comments.push(parsedQuery);
-        send(res, comments);
-    } else if (pathname == '/getComments') {
-        send(res, comments);
-    } else {
-        fs.readFile(__dirname + pathname, function(err, data) {
-            if (err) {
-                send(res, { message: 'Error' });
-                return;
-            }
-            res.writeHead(200);
-            res.end(data);
+    if (resourseName === 'comments' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        res.end(JSON.stringify(comments));
+    } else if (resourseName === 'comments' && req.method === 'POST') {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
         });
+        req.on('end', () => {
+            const bodyParsed = qs.parse(body);
+            comments.push({name: bodyParsed.name, comment: bodyParsed.comment});
+            fs.writeFile('./comments.json', JSON.stringify(comments), 'utf8', (err) => {
+                if (err) {
+                    res.end('Error');
+                } else {
+                    res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'});
+                    res.end('Success');
+                }
+            });
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end('Not Found');
     }
 });
 
-server.listen(3000, function() {
-    console.log('Server is running at http://localhost:3000');
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
